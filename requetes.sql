b7ec9b65b6962de146e5b9de97096ba7d333cdf8
SOSO30 <makiesama16@gmail.com>
2021-08-31 16:22:53 +0200

    insert et jointure

--- requetes.sql
+++ requetes.sql
@@ -5,8 +5,15 @@ SELECT * FROM Utilisateur WHERE `MDP` = "salut" AND `Identifiant`= "coucou"
 INSERT INTO Particulier (numero,Nom,Prenom,NumeroInscription) VALUES ("1","Jean","michel","12");
 INSERT INTO  Telephone (NumTel,TypeNumTel,numero) VALUES ("0687853089","portable","1");
 INSERT INTO AdressePostal (AdressePostal,NomAdressePostal,CodePostal,Ville,numero) VALUES("6 Rue Denie Papin","Etage1","51100","Reims","1");
-INSERT INTO Mail (id,AdresseElectronique) VALUES ("1","soso12@gmail.com");
+INSERT INTO Mail (id,AdresseElectronique,numero) VALUES ("1","soso12@gmail.com","1");
 
 #entreprise 
-INSERT INTO Entreprise (EntrepriseSiret,NumTel,TypeNumTel,AdressePostal,NomAdressePostal,CodePostal,CodePostal,Ville,NumeroTVA,EntrepriseNom,urlSite,AdresseElectronique,NumeroInscription)
-VALUES ("123 456 789 00013","0746980876","fixe","5 Rue Epynalt","Garage","51430","Tinqueux","FR 89 128370925",)
+INSERT INTO Entreprise (EntrepriseSiret,NumTel,TypeNumTel,AdressePostal,NomAdressePostal,CodePostal,Ville,NumeroTVA,EntrepriseNom,urlSite,AdresseElectronique,NumeroInscription)
+VALUES ("123 456 789 0001,"0746980876","fixe","5 Rue Epynalt","Garage","51430","Tinqueux","FR 89 128370925","Gilbert Entreprise","Gilbert-entreprise.fr","gilbertentreprise@gmail.com","13");
+
+#afficher les particulier avec leurs telephones,leurs adresses, et leur mail
+
+SELECT * FROM Particulier 
+left join Telephone on Telephone.numero=Particulier.numero 
+left join AdressePostal on AdressePostal.numero=Particulier.numero
+left join Mail on Mail.numero=Particulier.numero;
